ea7ee6696dd17e27610fa60f3257f0f1a8c8fa1e
Massimiliano Randazzo <massimiliano.randazzo@gmail.com>
2020-04-13 11:32:10 +0200

    Implementato il log4j2

--- src/main/java/mx/randalf/configuration/Configuration.java
+++ src/main/java/mx/randalf/configuration/Configuration.java
@@ -16,9 +16,10 @@ import java.util.List;
 import java.util.Properties;
 import java.util.Vector;
 
-import mx.randalf.configuration.exception.ConfigurationException;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
-import org.apache.log4j.Logger;
+import mx.randalf.configuration.exception.ConfigurationException;
 
 /**
  * Questa classe viene utilizzata per la gestione dell'apertura deil file comuni
@@ -32,7 +33,7 @@ public class Configuration {
 	/**
 	 * Questa variabile viene utilizzata per gestire il log dell'applicazione
 	 */
-	private static Logger log = Logger.getLogger(Configuration.class);
+	private static Logger log = LogManager.getLogger(Configuration.class);
 
 	/**
 	 * Questa variabile viene utilizzata per gestire la lista dei parametri
